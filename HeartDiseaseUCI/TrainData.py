d672bb2edf62da0d9bd84bb74c81d2c77057312b
hafezmehrjoo <hafezmehrjoo@gmail.com>
2020-08-26 18:21:09 +0430

    change train model in Disease (descend layer and add epochs and batch size)

--- HeartDiseaseUCI/TrainData.py
+++ HeartDiseaseUCI/TrainData.py
@@ -1,23 +1,20 @@
 import keras.layers as layer
+import keras.models as models
 
 
-def train_model(kereas_model, train_x, train_y, test_x, test_y):
-    model = kereas_model.Sequential()
+def train_model(x_train, y_train, x_test, y_test):
+    model = models.Sequential()
     model.add(layer.Dense(units=64, input_dim=13, activation='relu'))
     model.add(layer.Dense(units=64, activation='relu'))
     model.add(layer.Dense(units=64, activation='relu'))
-    model.add(layer.Dense(units=64, activation='relu'))
-    model.add(layer.Dense(units=64, activation='relu'))
-    model.add(layer.Dense(units=64, activation='relu'))
-    model.add(layer.Dense(units=64, activation='relu'))
-    model.add(layer.Dense(units=64, activation='relu'))
-    model.add(layer.Dense(units=64, activation='relu'))
-    model.add(layer.Dense(units=64, activation='relu'))
-    model.add(layer.Dense(units=64, activation='relu'))
+    model.add(layer.Dense(units=32, activation='relu'))
+    model.add(layer.Dense(units=32, activation='relu'))
+    model.add(layer.Dense(units=16, activation='relu'))
+    model.add(layer.Dense(units=8, activation='relu'))
     model.add(layer.Dense(units=1, activation='sigmoid'))
-    model.compile(loss='binary_crossentropy', optimizer='rmsprop', metrics=['acc'])
-    history = model.fit(train_x, train_y, epochs=25, batch_size=1, validation_split=0.2)
-    acc = model.evaluate(test_x, test_y)
+    model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['acc'])
+    history = model.fit(x_train, y_train, epochs=200, batch_size=8, validation_split=0.2)
+    acc = model.evaluate(x_test, y_test)
     epochs = history.history['val_loss'].__len__()
-    print('Error Percentage', 100 - (acc[1] * 100))
+    print(f'acc: {acc[1] * 100}')
     return model, history, epochs
