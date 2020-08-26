0c43b85d2b63a7b1d0ae66c93fb68b78c117ba20
hafezmehrjoo <hafezmehrjoo@gmail.com>
2020-08-26 18:22:26 +0430

    drop PredictionData and fix name of train test split in main.py HeartDiseaseUCI

--- HeartDiseaseUCI/main.py
+++ HeartDiseaseUCI/main.py
@@ -1,11 +1,8 @@
 from HeartDiseaseUCI import GetData
 from HeartDiseaseUCI import TrainData
-from HeartDiseaseUCI import PredictionModel
 from HeartDiseaseUCI import DataAnalysis
 import keras.models as model
 
 if __name__ == '__main__':
-    train_x, train_y, test_x, test_y = GetData.get_data(percent=90)
-    model_trained, model_history, epochs = TrainData.train_model(model, train_x, train_y, test_x, test_y)
-    print(PredictionModel.prediction_model(model=model_trained, count=303))
-    DataAnalysis.evaluate_best_epoch(history=model_history, epochs=epochs)
+    x_train, x_test,y_train , y_test = GetData.get_data(percent=90)
+    model_trained, model_history, epochs = TrainData.train_model(x_train, y_train, x_test, y_test)
