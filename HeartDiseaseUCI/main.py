90583eb61fb765d77eca7ec8ab91d0584462ee01
hafezmehrjoo <hafezmehrjoo@gmail.com>
2020-08-30 11:23:56 +0430

    add DataAnalysis module for model in main module

--- HeartDiseaseUCI/main.py
+++ HeartDiseaseUCI/main.py
@@ -1,8 +1,8 @@
 from HeartDiseaseUCI import GetData
 from HeartDiseaseUCI import TrainData
 from HeartDiseaseUCI import DataAnalysis
-import keras.models as model
 
 if __name__ == '__main__':
-    x_train, x_test,y_train , y_test = GetData.get_data(percent=90)
+    x_train, x_test, y_train, y_test = GetData.get_data(percent=90)
     model_trained, model_history, epochs = TrainData.train_model(x_train, y_train, x_test, y_test)
+    DataAnalysis.evaluate_best_epoch(history=model_history, epochs=epochs)
